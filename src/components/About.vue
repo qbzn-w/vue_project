6b48fb386934ea9e11c0c9d7fcb45295fc3e9768
qbzn-w <18149542718@163.com>
2022-05-16 11:29:00 +0800

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>about</h2>
+    <h3>{{this.$route.params.id}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <button v-on:click="goabout()">Add 1</button>
+    <button v-on:click="goparam()">goparam</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    goabout: function () {
+      this.$router.push('/about')
+    },
+    goparam: function () {
+      this.$router.push({name: 'about', params: {id: '1000'}})
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
